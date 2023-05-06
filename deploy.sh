ab121ed883ab9b398e39aaa4cda487fe281dee20
Ícaro Moura <irfm1@ifal.edu.br>
2023-05-06 13:23:49 -0300

    Teste vue

--- deploy.sh
+++ deploy.sh
@@ -18,6 +18,6 @@ git commit -m 'deploy'
 
 # 4. If you are deploying to https://<USERNAME>.github.io
 
-git push -f git@github.com:irfm1/irfm1.github.io.git main:gh-pages
+git push -f git@github.com:irfm1/mypage.git main:gh-pages
 
 cd -
